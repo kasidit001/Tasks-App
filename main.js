3255accd9ba4175b31eea1679394fab40af72550
Kasidit <kasiditdoctype2003@gmail.com>
2025-06-23 12:42:00 +0700

    remove unused POST route from main.js

--- main.js
+++ main.js
@@ -6,10 +6,6 @@ app.get("/", (req, res) => {
   app.send("Hello");
 });
 
-app.post("/", (req, res) => {
-  res.send(req.body);
-});
-
 app.listen(3000, () => {
   console.log("Start server is port 3000");
 });
